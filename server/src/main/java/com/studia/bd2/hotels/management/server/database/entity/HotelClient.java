5c61e205c8d0bc4e1edf41d5668dcc855f77c522
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-04-16 23:33:00 +0200

    Updated mapping for HotelClient

--- server/src/main/java/com/studia/bd2/hotels/management/server/database/entity/HotelClient.java
+++ server/src/main/java/com/studia/bd2/hotels/management/server/database/entity/HotelClient.java
@@ -1,5 +1,6 @@
 package com.studia.bd2.hotels.management.server.database.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,8 +9,24 @@ import static javax.persistence.GenerationType.AUTO;
 
 @Entity(name = "clients")
 public class HotelClient {
+
     @Id
     @GeneratedValue(strategy = AUTO)
+    @Column(name = "client_id")
     private Long id;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "is_conference")
+    private Boolean conference;
 }
