ce066bedcb0b768f84373442b599c2facdfd27ca
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-06-11 00:58:54 +0200

    Fix query

--- tools/benchmark/queries/is_a_room_in_the_hotel_free.sql
+++ tools/benchmark/queries/is_a_room_in_the_hotel_free.sql
@@ -1,11 +1,14 @@
-SELECT DISTINCT rooms.*
+SELECT rooms.*
 FROM hotels
   JOIN buildings ON hotels.id = buildings.hotel_id
-  JOIN rooms ON buildings.id = rooms.building_id
-  JOIN rooms_reservations ON rooms.id = rooms_reservations.room_id
-  JOIN reservations ON rooms_reservations.reservation_id = reservations.id
-WHERE ((reservations.check_in_date <= CURRENT_DATE AND
-        reservations.check_out_date <= DATE_ADD(CURRENT_DATE, INTERVAL 10 DAY)) OR
-       (reservations.check_in_date >= CURRENT_DATE AND
-        reservations.check_out_date >= DATE_ADD(CURRENT_DATE, INTERVAL 10 DAY))) AND
-      hotels.id = 1;
+  JOIN rooms on buildings.id = rooms.building_id
+WHERE
+  rooms.id NOT IN (
+    SELECT rooms.id
+    FROM rooms
+      JOIN rooms_reservations ON rooms.id = rooms_reservations.room_id
+      JOIN reservations ON rooms_reservations.reservation_id = reservations.id
+    WHERE
+      CURRENT_DATE BETWEEN reservations.check_in_date AND reservations.check_out_date
+  ) AND
+  hotels.id = 1;
